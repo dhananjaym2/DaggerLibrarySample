5d02ad24256fb6ceaafd698b45dab9f87288af26
Dhananjay <dhananjaymohnot@gmail.com>
2017-04-10 11:56:50 +0530

    incomplete changes in DomainModule.java for SessionManager and the code for this is commented

---
+++ app/src/main/java/com/daggerlibrarysample/domain/DomainModule.java
@@ -0,0 +1,19 @@
+package com.daggerlibrarysample.domain;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by Admin on 29-11-2016.
+ */
+
+@Module(
+        complete = false,
+        library = true
+)
+public class DomainModule {
+//http://antonioleiva.com/dagger-android-part-2/
+//    @Provides @Singleton public SessionManager// TODO incomplete
+}
